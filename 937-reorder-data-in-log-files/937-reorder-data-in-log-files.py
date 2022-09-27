063920a44fd5565cb10640de492bf0357c5c2fa1
Woobin Park <104637982+wbin0718@users.noreply.github.com>
2022-09-27 12:55:40 +0900

    Time: 39 ms (93.15%), Space: 14.1 MB (39.17%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,6 +1,7 @@
 import re
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        
         let, dig = [], []
         for log in logs:
             if log.split()[1].isdigit():
@@ -11,5 +12,3 @@ class Solution:
         let.sort(key = lambda x : (x.split()[1:],x.split()[0]))
                  
         return let + dig
-        
-        
