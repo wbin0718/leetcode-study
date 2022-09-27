5cd5d00eaa087f53259861728c4a696361165158
Woobin Park <104637982+wbin0718@users.noreply.github.com>
2022-09-27 15:39:07 +0900

    Time: 170 ms (35.83%), Space: 15.2 MB (24.48%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums = [(value,i) for i,value in enumerate(nums)]
+        nums = sorted(nums, key=lambda x : x[0])
+        left = 0
+        right = len(nums) - 1
         
-        graph = {value:key for key,value in enumerate(nums)}
-        for i,value in enumerate(nums):
-            if (target - value) in graph and i != graph[(target-value)]:
-                return [i, graph[(target-value)]]
-        
+        while left < right :
+            
+            tmp = nums[left][0] + nums[right][0]
+            if target == tmp:
+                return [min(nums[left][1], nums[right][1]), max(nums[left][1],nums[right][1])]
+            elif tmp > target:
+                right -= 1
+            elif tmp < target:
+                left += 1
